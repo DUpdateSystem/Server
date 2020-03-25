ec96b80cd101c9a16ab2978603cd4413b6a04417
xz-dev <xiangzhedev@gmail.com>
2020-03-25 15:17:23 +0800

    不处理错误请求

--- app/main.py
+++ app/main.py
@@ -2,18 +2,21 @@ import os
 import sys
 
 import json
-from requests.exceptions import RequestException
 
 from flask import Flask, request, jsonify
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.server.manager.data_manager import data_manager
+from app.server.hubs.library.hub_list import hub_dict
 
 app = Flask(__name__)
 
 
 @app.route('/v1/<hub_uuid>')
 def get_update_by_hub_uuid(hub_uuid: str):
+    if hub_uuid not in hub_dict:
+        print(f"NO HUB: {hub_uuid}")
+        return "", 400
     app_info_list = json.loads(request.headers.get("App-Info-List"))
     return_list = []
     for app_info in app_info_list:
