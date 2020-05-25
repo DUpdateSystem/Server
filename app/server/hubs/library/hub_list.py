19b7941dc346f245e76111b0f9002745bc4b7f27
xz-dev <xiangzhedev@gmail.com>
2020-05-25 19:07:54 +0800

    feat: 添加 Xposed Module Repository 支持

--- app/server/hubs/library/hub_list.py
+++ app/server/hubs/library/hub_list.py
@@ -3,11 +3,13 @@ from .coolapk import CoolApk
 from .fdroid import FDroid
 from .github import Github
 from .sjly import Sjly
+from .xp_mod_repo import XpModRepo
 
 hub_dict = {
     "fd9b2602-62c5-4d55-bd1e-0d6537714ca0": Github(),
     "6a6d590b-1809-41bf-8ce3-7e3f6c8da945": FDroid(),
     "1c010cc9-cff8-4461-8993-a86cd190d377": CoolApk(),
     "4a23c3a5-8200-40bb-b961-c1bb5d7fd921": AppChina(),
-    "1c010cc9-cff8-4461-8993-a86mm190d377": Sjly()
+    "1c010cc9-cff8-4461-8993-a86mm190d377": Sjly(),
+    "e02a95a2-af76-426c-9702-c4c39a01f891": XpModRepo()
 }
---
+++ app/server/hubs/library/xp_mod_repo.py
@@ -0,0 +1,60 @@
+import gzip
+import tempfile
+from xml.etree import ElementTree
+
+from bs4 import BeautifulSoup
+
+from ..base_hub import BaseHub
+from ..hub_script_utils import get_response, get_value_from_app_id
+from ...manager.cache_manager import cache_manager
+
+cache_key = "xposed_full_module_xml_file_bytes"
+
+
+class XpModRepo(BaseHub):
+    def get_release_info(self, app_id: list) -> tuple or None:
+        package = _get_package(app_id)
+        if package is None:
+            return None
+        raw = None
+        try:
+            raw = cache_manager.get_tmp_cache(cache_key)
+        except KeyError:
+            pass
+        if not raw:
+            raw = get_response("https://dl-xda.xposed.info/repo/full.xml.gz", stream=True).raw.data
+            if raw:
+                cache_manager.add_tmp_cache(cache_key, raw)
+        with tempfile.TemporaryFile(mode='w+b') as f:
+            f.write(raw)
+            f.flush()
+            f.seek(0)
+            with gzip.GzipFile(mode='r', fileobj=f) as gzip_file:
+                tree = ElementTree.parse(gzip_file)
+        module = tree.find(f'//module[@package="{package}"]')
+        version_list = module.findall('version')
+        data = []
+        for i in range(len(version_list)):
+            version = version_list[i]
+            download_url = version.find('download').text
+            file_name = download_url[download_url.rfind('/') + 1:]
+            change_log_raw = version.find('changelog').text
+            if change_log_raw:
+                soup = BeautifulSoup(change_log_raw, "html5lib")
+                change_log = soup.text
+            else:
+                change_log = None
+            release_info = {
+                "version_number": version.find('name').text,
+                "change_log": change_log,
+                "assets": [{
+                    "file_name": file_name,
+                    "download_url": download_url
+                }]
+            }
+            data.append(release_info)
+        return data
+
+
+def _get_package(app_info: list) -> str or None:
+    return get_value_from_app_id(app_info, "android_app_package")
