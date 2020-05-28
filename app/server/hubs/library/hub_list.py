2ed302cbec58a229a000a7613a797279a503fed0
xz-dev <xiangzhedev@gmail.com>
2020-05-28 13:14:27 +0800

    feat: 添加 Gitlab 的 Release 标签支持
    因缺乏可参考项目，暂不支持 Tag 标签
    close DUpdateSystem/UpgradeAll#2

--- app/server/hubs/library/hub_list.py
+++ app/server/hubs/library/hub_list.py
@@ -2,11 +2,13 @@ from .appchina import AppChina
 from .coolapk import CoolApk
 from .fdroid import FDroid
 from .github import Github
+from .gitlab import Gitlab
 from .sjly import Sjly
 from .xp_mod_repo import XpModRepo
 
 hub_dict = {
     "fd9b2602-62c5-4d55-bd1e-0d6537714ca0": Github(),
+    "a84e2fbe-1478-4db5-80ae-75d00454c7eb": Gitlab(),
     "6a6d590b-1809-41bf-8ce3-7e3f6c8da945": FDroid(),
     "1c010cc9-cff8-4461-8993-a86cd190d377": CoolApk(),
     "4a23c3a5-8200-40bb-b961-c1bb5d7fd921": AppChina(),
