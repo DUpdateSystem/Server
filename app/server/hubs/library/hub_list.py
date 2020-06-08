54248f370c5495806958f9c34f58d6405a931b45
xz-dev <xiangzhedev@gmail.com>
2020-06-08 11:31:50 +0800

    feat: 添加 Z直播 支持

--- app/server/hubs/library/hub_list.py
+++ app/server/hubs/library/hub_list.py
@@ -5,6 +5,7 @@ from .github import Github
 from .gitlab import Gitlab
 from .sjly import Sjly
 from .xp_mod_repo import XpModRepo
+from .zlive_official import ZLiveOfficial
 
 hub_dict = {
     "fd9b2602-62c5-4d55-bd1e-0d6537714ca0": Github(),
@@ -13,5 +14,6 @@ hub_dict = {
     "1c010cc9-cff8-4461-8993-a86cd190d377": CoolApk(),
     "4a23c3a5-8200-40bb-b961-c1bb5d7fd921": AppChina(),
     "1c010cc9-cff8-4461-8993-a86mm190d377": Sjly(),
-    "e02a95a2-af76-426c-9702-c4c39a01f891": XpModRepo()
+    "e02a95a2-af76-426c-9702-c4c39a01f891": XpModRepo(),
+    "28591e65-849f-4417-aead-9d9098520eed": ZLiveOfficial()
 }
---
+++ app/server/hubs/library/zlive_official.py
@@ -0,0 +1,19 @@
+from ..base_hub import BaseHub
+from ..hub_script_utils import get_response
+
+
+class ZLiveOfficial(BaseHub):
+    def get_release_info(self, app_id: list) -> tuple or None:
+        data = get_response(_get_url())
+        return [{
+            "version_number": data["version_name"],
+            "change_log": data["changelog"],
+            "assets": [{
+                "file_name": f'zlive_{data["version_name"]}.apk',
+                "download_url": data["url"]
+            }]
+        }]
+
+
+def _get_url() -> str:
+    return "https://zlive.linroid.com/api/app_version"
