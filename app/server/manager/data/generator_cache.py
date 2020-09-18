ec27d2f88cd7371890e0842e6c99ee7181e1062d
xz-dev <xiangzhedev@gmail.com>
2020-09-18 11:34:18 +0800

    fix: 使用 list 替代 queue

--- app/server/manager/data/generator_cache.py
+++ app/server/manager/data/generator_cache.py
@@ -1,24 +1,36 @@
-from multiprocessing import SimpleQueue
+from asyncio import Event
+
+from app.server.utils import set_new_asyncio_loop, call_def_in_loop_return_result
 
 
 class GeneratorCache:
 
     def __init__(self):
-        self.__queue = SimpleQueue()
+        self.__loop = set_new_asyncio_loop()
+        self.__cache_queue = []
+        self.__next_lock = Event()
         self.__closed = False
 
     def close(self):
+        self.__cache_queue.append(None)
         self.__closed = True
-        self.__queue.put(None)
 
     def add_value(self, value):
-        self.__queue.put(value)
+        self.__cache_queue.append(value)
+        self.__next_lock.set()
 
     def __next__(self):
-        value = self.__queue.get()
-        if value is None and self.__closed:
+        v = call_def_in_loop_return_result(self.__get_next_item(), self.__loop)
+        if self.__closed and v is None:
             raise StopIteration
-        return value
+        return v
 
     def __iter__(self):
         return self
+
+    async def __get_next_item(self):
+        await self.__next_lock.wait()
+        v = self.__cache_queue.pop(0)
+        if not self.__cache_queue:
+            self.__next_lock.clear()
+        return v
