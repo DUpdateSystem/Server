67498e2baa7b333a9401162c0b38d199f7827d92
xz-dev <xiangzhedev@gmail.com>
2020-09-22 14:03:02 +0800

    fix: 暂时停用酷安源

--- app/server/hubs/hub_list.py
+++ app/server/hubs/hub_list.py
@@ -13,7 +13,6 @@ hub_dict = {
     "a84e2fbe-1478-4db5-80ae-75d00454c7eb": Gitlab(),
     "6a6d590b-1809-41bf-8ce3-7e3f6c8da945": FDroid(),
     "65c2f60c-7d08-48b8-b4ba-ac6ee924f6fa": GooglePlay(),
-    "1c010cc9-cff8-4461-8993-a86cd190d377": CoolApk(),
     "4a23c3a5-8200-40bb-b961-c1bb5d7fd921": AppChina(),
     "1c010cc9-cff8-4461-8993-a86mm190d377": Sjly(),
     "e02a95a2-af76-426c-9702-c4c39a01f891": XpModRepo(),
