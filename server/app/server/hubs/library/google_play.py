24f21d527a37a244219b35be72636053e7f39332
xz-dev <xiangzhedev@gmail.com>
2020-10-17 17:35:59 +0800

    fix: 避免 Google Play Token 过期后崩溃的问题

--- server/app/server/hubs/library/google_play.py
+++ server/app/server/hubs/library/google_play.py
@@ -32,7 +32,13 @@ class GooglePlay(BaseHub):
         api.setAuthSubToken(auth_sub_token)
         [return_value_no_break(generator_cache, app_id, []) for app_id in app_id_list if android_app_key not in app_id]
         package_list = [app_id[android_app_key] for app_id in app_id_list if android_app_key in app_id]
-        bulk_details = api.bulkDetails(package_list)
+        try:
+            bulk_details = api.bulkDetails(package_list)
+        except RequestError:
+            gsf_id, auth_sub_token = self.__get_new_token()
+            api.gsfId = gsf_id
+            api.setAuthSubToken(auth_sub_token)
+            bulk_details = api.bulkDetails(package_list)
         for i, l_details in enumerate(bulk_details):
             app_id = app_id_list[i]
             if l_details is None:
@@ -92,7 +98,7 @@ class GooglePlay(BaseHub):
             auth_json = json.loads(auth)
             if not self.__check_health_limit_num(auth_json):
                 if self.__check_token(auth_json):
-                    self.__set_auth_limit_num(auth_json, 50)
+                    self.__set_auth_limit_num(auth_json, 25)
                     return auth_json
                 else:
                     return self.__get_new_token()
@@ -139,7 +145,7 @@ class GooglePlay(BaseHub):
             "mail": "xiangzhedev@gmail.com",
             "passwd": "slzlpcugmdydxvii",
         })
-        self.__set_auth_limit_num(auth_json, 50)
+        self.__set_auth_limit_num(auth_json, 25)
         logging.info("GooglePlay: Renew Token")
         return auth_json
 
