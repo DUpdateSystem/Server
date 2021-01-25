de26e5ffb4af6877d738ac93a9466dcf03544898
忘却的旋律 <51457212+JohnsonRan@users.noreply.github.com>
2021-01-25 09:58:32 +0800

    feat: 添加Github下载加速源

---
+++ server/app/server/hubs/library/GithubProxy.py
@@ -0,0 +1,17 @@
+from .github import Github
+from ..base_hub import BaseHub
+
+
+class GithubProxy(Github, BaseHub):
+    """GitHub 下载加速源"""
+
+    @staticmethod
+    def get_uuid() -> str:
+        return '56fca689-47a7-496a-b290-8bd717c06960'
+
+    def get_release(self, app_id: dict, auth: dict or None = None) -> list:
+        data = super().get_release(app_id, auth)
+        for release in data:
+            for asset in release["assets"]:
+                asset["download_url"] = f'https://git.johnsonran.cn/{asset["download_url"]}'
+        return data
