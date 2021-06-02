df5d070d087bbea7bb5b50b2d7a16fbe9b9bda37
banbendalao <291490382@qq.com>
2021-06-02 14:10:50 +0800

    添加docker多架构自动构建支持，减小镜像体积，更换基础镜像

--- server/oci_build/DockerfileRUN.sh
+++ server/oci_build/DockerfileRUN.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
-apk add --no-cache --virtual .build-deps \
-	build-base python3-dev git linux-headers \
-	libressl-dev musl-dev libffi-dev cargo
+#!/bin/bash
+apt-get update
+apt-get install -y python3-dev git musl-dev libffi-dev cargo protobuf-compiler libssl-dev
 pip install --no-cache-dir -r requirements.txt
-python -m pip install 'requests[socks]'
-apk add --no-cache protoc
-pip install setuptools-rust
 git clone --depth=1 https://github.com/NoMore201/googleplay-api
-cd ./googleplay-api && python setup.py build && python setup.py install
+cd googleplay-api
+python setup.py build
+python setup.py install
 pip uninstall -y setuptools-rust semantic_version toml
+cd ..
 rm -rf ./googleplay-api
-apk del .build-deps
-apk add --no-cache libstdc++ libressl-dev
+apt-get autoremove --purge -y python3-dev git musl-dev libffi-dev cargo
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+rm -rf /tmp
