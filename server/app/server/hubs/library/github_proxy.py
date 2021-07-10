4e09827df74a538a6853e04d41d52a9089bebe87
xz-dev <xiangzhedev@gmail.com>
2021-07-10 22:03:17 +0800

    perf: 切换 Github 的加速服务商

--- server/app/server/hubs/library/github_proxy.py
+++ server/app/server/hubs/library/github_proxy.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from .github import Github
 from ..base_hub import BaseHub
 from ..hub_script_utils import get_release_by_uuid
@@ -5,6 +7,7 @@ from ..hub_script_utils import get_release_by_uuid
 
 class GithubProxy(BaseHub):
     """GitHub 下载加速源"""
+
     @staticmethod
     def get_uuid() -> str:
         return '56fca689-47a7-496a-b290-8bd717c06960'
@@ -15,6 +18,13 @@ class GithubProxy(BaseHub):
         for release in data:
             for asset in release["assets"]:
                 url = asset["download_url"]
-                asset["download_url"] = f'http://git.521331.xyz/{url}'
-                # Github 下载加速服务，感谢 忘却的旋律.
+                asset["download_url"] = self.__get_proxy_url(url)
+                # Github 下载加速服务，感谢 JohnsonRan 的发现与 FastGit 提供下载服务.
+                # FastGit 文档/捐赠：https://doc.fastgit.org/zh-cn/#%E5%85%B3%E4%BA%8E-fastgit
         return data
+
+    @staticmethod
+    def __get_proxy_url(old_url: str) -> str:
+        parsed = urlparse(old_url)
+        replaced = parsed._replace(netloc='download.fastgit.org')
+        return replaced.geturl()
