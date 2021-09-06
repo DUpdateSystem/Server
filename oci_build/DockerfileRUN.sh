862e00003c117c25ab9aaa5858f93d99118a7ce6
xz-dev <xiangzhedev@gmail.com>
2021-09-06 14:12:59 +0800

    ci: 镜像构建脚本执行失败后立即停止构建

--- oci_build/DockerfileRUN.sh
+++ oci_build/DockerfileRUN.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e  # This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
+
 apt-get update
 apt-get install -y python3-dev git musl-dev libffi-dev cargo protobuf-compiler libssl-dev
 pip install --no-cache-dir -r requirements.txt
@@ -13,3 +16,4 @@ apt-get autoremove --purge -y python3-dev git musl-dev libffi-dev cargo
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
+
