67505d21cd78868c4115245da4c6fccb831c394e
xz-dev <xiangzhedev@gmail.com>
2021-09-06 15:22:50 +0800

    fix: 修复 getter 超时函数没有返回值的问题

--- server/utils/asyncio.py
+++ server/utils/asyncio.py
@@ -5,6 +5,6 @@ from utils.logging import logging
 
 async def run_with_time_limit(aw, timeout: int):
     try:
-        await asyncio.wait_for(aw, timeout=timeout)
+        return await asyncio.wait_for(aw, timeout=timeout)
     except asyncio.TimeoutError:
         logging.info(f'aw: {aw} timeout!')
