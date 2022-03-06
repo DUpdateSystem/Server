2c9692c4bfb056c03fcfaec5151614169c10a3e4
xz-dev <xiangzhedev@gmail.com>
2022-03-06 16:54:55 +0800

    ci(deps): Add cmake as pynng build dependencies

--- oci_build/DockerfileRUN.sh
+++ oci_build/DockerfileRUN.sh
@@ -3,10 +3,10 @@
 set -e  # This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
 
 apt-get update
-apt-get install -y python3-dev git musl-dev libffi-dev cargo protobuf-compiler libssl-dev
+apt-get install -y python3-dev git musl-dev libffi-dev cargo protobuf-compiler libssl-dev cmake
 pip install --no-cache-dir -r requirements.txt
 pip uninstall -y setuptools-rust semantic_version
-apt-get autoremove --purge -y python3-dev git musl-dev libffi-dev cargo
+apt-get autoremove --purge -y python3-dev git musl-dev libffi-dev cargo cmake
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
