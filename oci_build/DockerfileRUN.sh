523b42fa3d125697d23d79800c878450d5efb2c8
xz-dev <xiangzhedev@gmail.com>
2022-06-05 00:01:39 +0800

    fix(deps): replace googleplay-api with marty0678/googleplay-api

--- oci_build/DockerfileRUN.sh
+++ oci_build/DockerfileRUN.sh
@@ -5,6 +5,11 @@ set -e  # This will cause the shell to exit immediately if a simple command exit
 apt-get update
 apt-get install -y python3-dev git musl-dev libffi-dev cargo protobuf-compiler libssl-dev cmake
 pip install --no-cache-dir -r requirements.txt
+git clone --depth 1 https://github.com/marty0678/googleplay-api.git \
+    && cd googleplay-api \
+    && python setup.py install \
+    && cd .. \
+    && rm -rf googleplay-api
 pip uninstall -y setuptools-rust semantic_version
 apt-get autoremove --purge -y python3-dev git musl-dev libffi-dev cargo cmake
 apt-get clean
